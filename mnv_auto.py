0d1c3ae8a3e602c8d4637c21b22fb4b98d3ff686
Tornado3P9 <max.barzantny@yahoo.de>
2025-03-04 14:23:21 +0100

    20250304

--- mnv_auto.py
+++ mnv_auto.py
@@ -1,16 +1,38 @@
 import krpc
 import time
 import math
+import argparse
 
-def execute_maneuver_node():
+def str2bool(v: str) -> bool:
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def commandLine() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description = "Mun lander script")
+    parser.add_argument("-V", "--version", action='version', version='%(prog)s 1.0')
+    parser.add_argument('--do_circle', type=str2bool, nargs='?', const=True, default=False, help='Automatically create a maneuver node to circularize at apoapsis instead of creating a maneuver node manually (default: False)')
+    return parser.parse_args()
+
+def execute_maneuver_node(do_circle):
     conn = krpc.connect(name='Maneuver Execution')
     vessel = conn.space_center.active_vessel
+    if do_circle:
+        planning_circularization_burn(conn, vessel)
+    if not vessel.control.nodes:
+        print("No maneuver node exists.")
+        return None
     node = vessel.control.nodes[0]
 
     start_time = calculate_start_time(conn, node)
 
     conn.space_center.warp_to(start_time - 40)
-    print("Waiting until start time")
+    print("Waiting until maneuver start...")
     while conn.space_center.ut < start_time - 30:
         time.sleep(1)
 
@@ -39,6 +61,18 @@ def execute_maneuver_node():
     time.sleep(1)
     print("Script exited.")
 
+def planning_circularization_burn(conn, vessel):
+    print('Planning circularization burn')
+    mu = vessel.orbit.body.gravitational_parameter
+    r = vessel.orbit.apoapsis
+    a1 = vessel.orbit.semi_major_axis
+    a2 = r
+    v1 = math.sqrt(mu*((2./r)-(1./a1)))
+    v2 = math.sqrt(mu*((2./r)-(1./a2)))
+    delta_v = v2 - v1
+    vessel.control.add_node(conn.space_center.ut + vessel.orbit.time_to_apoapsis, prograde=delta_v)
+    time.sleep(1)
+
 def calculate_start_time(conn, node):
     global mnv_time
     mnv_time = maneuver_burn_time(conn, node)
@@ -51,9 +85,10 @@ def maneuver_burn_time(conn, node):
 
     engines = vessel.parts.engines
     active_engines = [engine for engine in engines if engine.active]
+    print("Available Engines:")
     for engine in active_engines:
         isp = engine.specific_impulse
-        print(f'Engine: {engine.part.title}, ISP: {isp}')
+        print(f'  Engine: {engine.part.title}, ISP: {isp}')
     total_isp = sum(engine.specific_impulse for engine in active_engines)
     # average_isp = total_isp / len(engines)
     isp = total_isp
@@ -62,7 +97,7 @@ def maneuver_burn_time(conn, node):
     fuel_flow = vessel.available_thrust / (isp * g0)
     t = (vessel.mass - mf) / fuel_flow
 
-    print(f"maneuverBurnTime: {round(t)}s.")
+    print(f"Maneuver duration: {round(t)}s.")
     return t
 
 def do_circ_countdown(conn, start_time):
@@ -102,10 +137,11 @@ def v_ang(v1, v2):
     return angle
 
 if __name__ == "__main__":
+    argument = commandLine()
     try:
-        execute_maneuver_node()
+        execute_maneuver_node(argument.do_circle)
     except KeyboardInterrupt:
-        print("Script exited.")
+        print("Script stopped by user.")  # Ctrl+c
 
 #########################################################
 # first_data = []
