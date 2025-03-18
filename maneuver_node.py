926407d668604828d874bf9385b640d11afce565
Tornado3P9 <max.barzantny@yahoo.de>
2025-03-18 18:23:31 +0100

    20250318

--- maneuver_node.py
+++ maneuver_node.py
@@ -4,29 +4,21 @@ import math
 import argparse
 
 
-def str2bool(v: str) -> bool:
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
 def commandLine() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description = "Execute Maneuver Node")
     parser.add_argument("-V", "--version", action='version', version='%(prog)s 1.0')
-    parser.add_argument('--do_circle', type=str2bool, nargs='?', const=True, default=False, help='Automatically create a maneuver node to circularize at apoapsis instead of creating a maneuver node manually (default: False)')
+    parser.add_argument('--circularize_at', choices=['ap', 'pe'], default=None, help="Choose either 'ap' or 'pe'.")
     return parser.parse_args()
 
 
-def execute_maneuver_node(do_circle) -> None:
+def execute_maneuver_node() -> None:
+    argument = commandLine()
     conn = krpc.connect(name='Maneuver Execution')
     vessel = conn.space_center.active_vessel
-    if do_circle:
-        planning_circularization_burn(conn, vessel)
+    vessel.control.sas = False
+    if argument.circularize_at:
+        planning_circularization_burn(conn, vessel, argument.circularize_at)
+    
     if not vessel.control.nodes:
         print("No maneuver node exists.")
         return None
@@ -34,24 +26,17 @@ def execute_maneuver_node(do_circle) -> None:
 
     start_time = calculate_start_time(conn, node)
 
-    conn.space_center.warp_to(start_time - 40)
-    print("Waiting until maneuver start...")
-    while conn.space_center.ut < start_time - 30:
-        time.sleep(1)
-
+    conn.space_center.warp_to(start_time - 30)
     vessel.auto_pilot.engage()
     vessel.auto_pilot.reference_frame = node.reference_frame
     vessel.auto_pilot.target_direction = node.burn_vector(node.reference_frame)
-    do_circ_countdown(conn, start_time)
+    print("Waiting until maneuver start...")
 
-    while conn.space_center.ut < start_time:
-        time.sleep(0.1)
+    do_circ_countdown(conn, start_time)
 
     vessel.control.throttle = 1.0
     
     original_vector = node.burn_vector(node.reference_frame)
-    # print(f"type of original_vector: {type(original_vector)}")
-    # print(f"original_vector: {original_vector}")
     print("Maneuver in progress...")
     while not is_maneuver_complete(vessel, original_vector):
         pass
@@ -65,16 +50,22 @@ def execute_maneuver_node(do_circle) -> None:
     print("Script exited.")
 
 
-def planning_circularization_burn(conn, vessel) -> None:
+def planning_circularization_burn(conn, vessel, option) -> None:
     print('Planning circularization burn')
     mu = vessel.orbit.body.gravitational_parameter
-    r = vessel.orbit.apoapsis
+    if option == 'ap':
+        r = vessel.orbit.apoapsis
+    elif option == 'pe':
+        r = vessel.orbit.periapsis
     a1 = vessel.orbit.semi_major_axis
     a2 = r
     v1 = math.sqrt(mu*((2./r)-(1./a1)))
     v2 = math.sqrt(mu*((2./r)-(1./a2)))
     delta_v = v2 - v1
-    vessel.control.add_node(conn.space_center.ut + vessel.orbit.time_to_apoapsis, prograde=delta_v)
+    if option == 'ap':
+        vessel.control.add_node(conn.space_center.ut + vessel.orbit.time_to_apoapsis, prograde=delta_v)
+    elif option == 'pe':
+        vessel.control.add_node(conn.space_center.ut + vessel.orbit.time_to_periapsis, prograde=delta_v)
     time.sleep(1)
 
 
@@ -103,7 +94,7 @@ def maneuver_burn_time(conn, node) -> float:
     fuel_flow = vessel.available_thrust / (isp * g0)
     t = (vessel.mass - mf) / fuel_flow
 
-    print(f"Maneuver duration: {round(t)}s.")
+    print(f"Maneuver duration: {t:.2f}s.")
     return t
 
 
@@ -147,12 +138,16 @@ def v_ang(v1, v2) -> float:
 
 
 if __name__ == "__main__":
-    argument = commandLine()
-    print("\033c", end="")
     try:
-        execute_maneuver_node(argument.do_circle)
+        print("\033c", end="")
+        execute_maneuver_node()
+    except ZeroDivisionError as e:
+        print(f"\nZeroDivisionError -> probably empty fuel tank or engine deactivated: {e}")
     except KeyboardInterrupt:
-        print("Script stopped by user.")  # Ctrl+c
+        print("\nScript exited by user.")
+    except Exception as e:
+        print(f"\nAn unexpected error occurred (note: keep navball extended): {e}")
+
 
 
 #########################################################
